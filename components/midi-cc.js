605ca37ad699834dfcd84742c0d0df104dbe7be9
Ruth John <rumyra@gmail.com>
2017-11-27 13:26:08 +0000

    eighty percent done

--- components/midi-cc.js
+++ components/midi-cc.js
@@ -11,37 +11,63 @@
 //       get: this.__getFromShadowRoot.bind(this)
 //     });
 //   }
-  
+
 //   __getFromShadowRoot (target, name) {
 //     return this.shadowRoot.querySelector('[ref="' + name + '"]');
 //   }
 // }
 
 // <my-el><span>here</span></my-el>
-  
+
 const containerTemplate = document.createElement('template');
 
 containerTemplate.innerHTML = `
   <style>
   .midi-control__group {
-    padding:2vm;
+    outline: 0;
+    padding: 3vmin;
+    display: grid;
+    grid-template-columns: 20% 80%;
+    align-items: center;
   }
   .midi-control__label {
+    outline: 0;
+    justify-self: center;
     display: inline-block;
-    width: 30%;
+    box-sizing: border-box;
   }
   .midi-control__input--slider,
   .midi-control__input--slider::-webkit-slider-thumblider-runnable-track {
+    outline: 0;
     display: inline-block;
-    margin: 0px auto;
+    margin: 21px auto;
     position: relative;
-    height: 24px; width: 68%;
+    height: 6px; width:100%;
     cursor: pointer;
-    background-color: black;
-    background-image:linear-gradient(90deg, black, fuchsia);
-    border: 1px solid fuchsia;
+    background-color: hsla(272, 94%, 70%, 1.0);
+    background-image: linear-gradient(90deg, hsla(272, 94%, 70%, 1.0), hsla(194, 89%, 56%, 1.0), hsla(150, 92%, 54%, 1.0));
+    /* border: 1px solid fuchsia; */
     border-radius: 2px;
+    box-shadow:
+      inset 1px 1px 1px hsla(0, 0%, 35%, 0.5),
+      1px 1px 1px #fff;
     -webkit-appearance: none;
+  }
+  .midi-control__input--slider::-webkit-slider-thumb {
+    -webkit-appearance: none;
+    box-sizing: border-box;
+    height: 42px;
+    width: 24px;
+    border: 2px solid;
+    border-image: linear-gradient(120deg, hsla(272, 94%, 70%, 1.0), hsla(194, 89%, 56%, 1.0), hsla(150, 92%, 54%, 1.0)) 5;
+    border-radius: 2px;
+    background-color: hsla(210, 25%, 98%, 1.0);
+    background-image:linear-gradient(0deg, hsla(210, 25%, 96%, 1.0), hsla(210, 25%, 98%, 1.0));
+    box-shadow:
+      1px 1px 4px 1px hsla(0, 0%, 35%, 0.4),
+      inset 2px 2px 0px 2px #fff,
+      inset -2px -2px 0px 2px hsla(210, 25%, 98%, 1.0)
+    ;
   }
   </style>
   <div class="midi-control__group">
@@ -53,7 +79,7 @@ containerTemplate.innerHTML = `
 class MidiCCController extends HTMLElementWithRefs {
   constructor() {
     super();
-    
+
     this.tabIndex = 0;
     this.attachShadow({mode: 'open'});
     this.shadowRoot.appendChild(containerTemplate.content.cloneNode(true));
@@ -68,7 +94,7 @@ class MidiCCController extends HTMLElementWithRefs {
     });
 
   }
-  
+
   static get observedAttributes() { return ['channel', 'value']; }
   attributeChangedCallback(attr, oldValue, newValue) {
 
